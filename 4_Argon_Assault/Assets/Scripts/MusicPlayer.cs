a08658c18d62aed44431a7d2ec8917f08ab17cca
Zaatar <hani.zaatar.92@gmail.com>
2021-01-01 02:55:32 +0200

    Removing unneeded assets prior to commit

---
+++ 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MusicPlayer : MonoBehaviour {
+    void Awake() 
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+}
