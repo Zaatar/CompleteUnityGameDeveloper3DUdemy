e02366feba1831d96f3f8c157e2c51b899907437
Zaatar <hani.zaatar.92@gmail.com>
2021-01-28 17:56:37 +0200

    Adding Get Closest Enemy functionality, finishing video 149

--- 5_Realm_Rush/Assets/Scripts/Tower.cs
+++ 5_Realm_Rush/Assets/Scripts/Tower.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.ParticleSystem;
 
 public class Tower : MonoBehaviour
 {
+    //Parameters
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
     [SerializeField] float shootingRange = 3f;
     [SerializeField] ParticleSystem projectileParticle;
 
+
+    //State
+    Transform targetEnemy;
     void PanTowardsEnemy()
     {
         objectToPan.transform.LookAt(targetEnemy);
@@ -41,6 +45,7 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SetTargetEnemy();
         if (targetEnemy)
         {
             PanTowardsEnemy();
@@ -49,5 +54,28 @@ public class Tower : MonoBehaviour
         {
             Shoot(false);
         }
+    }
+
+    private void SetTargetEnemy()
+    {
+        EnemyDamage[] enemyList = FindObjectsOfType<EnemyDamage>();
+        if(enemyList.Length == 0) { return; }
+        Transform closestEnemy = enemyList[0].transform;
+        foreach (EnemyDamage enemy in enemyList)
+        {
+            closestEnemy = GetClosest(closestEnemy, enemy.transform);
+        }
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        float distToA = Vector3.Distance(transform.position, transformA.position);
+        float distToB = Vector3.Distance(transform.position, transformB.position);
+        if(distToA > distToB)
+        {
+            return transformB;
+        }
+        return transformA;
     }
 }
