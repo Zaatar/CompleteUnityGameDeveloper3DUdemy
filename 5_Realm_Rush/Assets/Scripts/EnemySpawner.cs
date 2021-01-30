051dbab0037150ad52435481bc94369302523809
Zaatar <hani.zaatar.92@gmail.com>
2021-01-30 02:12:22 +0200

    Adding UI Canvas and Text Elements for Score and Base Health, video 160

--- 5_Realm_Rush/Assets/Scripts/EnemySpawner.cs
+++ 5_Realm_Rush/Assets/Scripts/EnemySpawner.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [Range(0.1f,120f)][SerializeField] float waitBeforeSpawn = 2f;
     [SerializeField] EnemyMovement objectToSpawn;
+    [SerializeField] Text scoreText;
+    int score = 0;
 
     private void Start()
     {
+        scoreText.text = score.ToString();
         StartCoroutine(SpawnEnemies());
     }
 
@@ -18,6 +22,8 @@ public class EnemySpawner : MonoBehaviour
         {
             EnemyMovement enemy = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
             enemy.transform.parent = this.transform;
+            score++;
+            scoreText.text = score.ToString();
             yield return new WaitForSeconds(waitBeforeSpawn);
         }
     }
---
+++ 5_Realm_Rush/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int baseHealth = 5;
+    [SerializeField] Text healthText;
+
+    private void Start()
+    {
+        healthText.text = baseHealth.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        baseHealth--;
+        healthText.text = baseHealth.ToString();
+    }
+}
